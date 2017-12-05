6837839cf06cf2de1989dab575755dc3b9e558d7
Christian Hansen <447922@rts-365.dk>
2017-12-05 12:56:11 +0100

    Update sql.js

--- api/config/sql.js
+++ api/config/sql.js
@@ -6,7 +6,7 @@ module.exports = {
             'host': 'localhost',
             'port': '3306',
             'user': 'root',
-            'password': '',
+            'password': 'oceantest1234',
             'database': 'digitalocean'
         });
     }
